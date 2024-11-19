ba808d0e54d268f8fd79b81ed7c5ca9294330a0f
SamSyntax <agniazdowski@gmail.com>
2024-11-19 15:44:31 +0100

    healthz?

--- internal/handlers/handler_readiness.go
+++ internal/handlers/handler_readiness.go
@@ -1,11 +1,16 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
-
-	"stulej-finder/internal/utils"
+	// "stulej-finder/internal/utils"
 )
 
 func HandlerReadiness(w http.ResponseWriter, r *http.Request) {
-	utils.RespondWithJSON(w, 200, struct{ Key string }{Key: "ok"})
+	// utils.RespondWithJSON(w, 200, struct{ Key string }{Key: "ok"})
+	w.WriteHeader(200)
+	_, err := w.Write([]byte("ok"))
+	if err != nil {
+		log.Fatalf("Failed to write header %v", err)
+	}
 }
